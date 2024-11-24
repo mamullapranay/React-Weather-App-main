a564596094d47e6d294e883b2cf68f97200aa752
pranay360 <mamullapranay360@gmail.com>
2024-11-24 17:40:33 +0530

    publish

--- React-Weather-App/src/components/Credit.js
+++ React-Weather-App/src/components/Credit.js
@@ -4,10 +4,6 @@ export default function Credit() {
   return (
     <div class="credit">
     <p>
-      Coded by
-      <a href="https://www.shecodes.io/graduates/73033-chaimae-lamirine"
-        > Chaimae Lamirine</a
-      >, <a href="https://github.com/shemmee/React-Weather-App">Open sourced on Github.</a> Hosted on Netlify
     </p>
   </div>
   );
